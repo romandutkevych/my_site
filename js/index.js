b9cda4d690e394d7f9157f35c0abcfd2adc7840f
Roman <roma3110@ukr.net>
2017-04-09 10:09:19 +0000

    form validation

--- js/index.js
+++ js/index.js
@@ -53,3 +53,59 @@ $(function(){
  
    
 });
+
+
+//form validation
+
+
+
+	$(function() {
+
+		$('.fr_contact').each(function(){
+			var form = $(this),
+				btn = form.find('.btn_submit')
+			
+			form.find('.form_field').addClass('empty_field');
+			
+			// Функция проверки полей формы
+			function checkInput(){
+				form.find('.form_field').each(function(){
+					if($(this).val() != ''){
+						$(this).removeClass('empty_field');
+					} else {
+						$(this).addClass('empty_field');
+					}
+				});
+			}
+			
+			// Функция подсветки незаполненных полей
+			function lightEmpty(){
+				form.find('.empty_field').css({'border-color':'#f20000'});
+				setTimeout(function(){
+					form.find('.empty_field').removeAttr('style');
+				},1000);
+			}
+			
+			setInterval(function(){
+				checkInput();
+				var sizeEmpty = form.find('.empty_field').size();
+				if(sizeEmpty > 0){
+					
+					btn.addClass('disabled');
+				}else{
+					btn.removeClass('disabled')
+				}
+			},500);
+
+			btn.click(function(){
+				if(btn.hasClass('disabled')){
+					lightEmpty();
+					return false;
+				} else {
+					form.submit();
+				}
+			});
+			
+		});
+		
+	});
